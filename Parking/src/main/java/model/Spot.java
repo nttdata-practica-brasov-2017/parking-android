37f992e06f93708f2451ca29dc28df0ceb1014df
Monica Valcu <m09ny@yahoo.com>
2017-09-13 14:24:49 +0300

    Added Spot

---
+++ Parking/src/main/java/model/Spot.java
@@ -0,0 +1,40 @@
+package model;
+
+/**
+ * Created by m09ny on 09/08/17.
+ */
+
+public class Spot {
+
+    private int spotNumber;
+    private int floor;
+
+    public Spot(int number, int floor) {
+        this.spotNumber = number;
+        this.floor = floor;
+    }
+
+    public int getNumber() {
+        return spotNumber;
+    }
+
+    public void setNumber(int number) {
+        this.spotNumber = number;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    @Override
+    public String toString() {
+        return "Spot{" +
+                "spotNumber=" + spotNumber +
+                ", floor=" + floor +
+                '}';
+    }
+}
