4a5bfc8cf8299aaa9eb57f6abf5fa39d7742c6f6
Raluca <Raluca_mih97@yahoo.com>
2017-09-26 11:21:15 +0300

    LoginDelegate modificat dar nu e varianta buna

--- Parking/src/main/java/webservice/LoginDelegate.java
+++ Parking/src/main/java/webservice/LoginDelegate.java
@@ -4,6 +4,7 @@ package webservice;
 import java.io.UnsupportedEncodingException;
 
 public interface LoginDelegate {
+
     public void onLoginDone(String result) throws UnsupportedEncodingException;
     public void onLoginError(String result);
 }
